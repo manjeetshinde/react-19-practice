2f7bcd29e2dc46d6c6f5782dda0f1d2a75b7ed53
Manjeet Shinde <manjeetshinde1996@gmail.com>
2024-12-29 11:05:03 +0530

    initialize React application with Pizza component and basic HTML structure

---
+++ App.js
@@ -0,0 +1,24 @@
+const Pizza = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, props.name),
+    React.createElement("p", {}, props.description),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Pixel Perfect Pizza"),
+    React.createElement(Pizza, {
+      name: "Pepperoni",
+      description: "Classic pepperoni pizza",
+    }),
+    React.createElement(Pizza, {
+      name: "Margherita",
+      description: "Simple and delicious",
+    }),
+  ]);
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
